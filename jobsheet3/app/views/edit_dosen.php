ad875befb0f288dc89a65dc5c7444fc901553ef8
av <alifiaafifa14@gmail.com>
2023-10-18 15:50:13 +0700

    Update edit_dosen.php

--- jobsheet3/app/views/edit_dosen.php
+++ jobsheet3/app/views/edit_dosen.php
@@ -36,44 +36,30 @@ $db = new database();
 <br>
 <h3>Edit Data Dosen</h3>
 <br>
+
 <form action="proses_dosen.php?aksi=update_dosen" method="post">
-
-
 <?php
 foreach ($db -> edit_dosen ($_GET['id']) as $d){
 ?>
-        <table>
-        <tr>
-            <td>NIDN</td>
-            <td>
-                <input type="hidden" name="id" value="<?php echo $d['id'] ?>">
-                <input type="text" name="nidn" value="<?php echo $d['nidn'] ?>">
-            </td>
-        </tr>
-        <tr>
-            <td>Nama</td>
-            <td><input type="text" name="nama" value="<?php echo $d['nama'] ?>"></td>
-        </tr>
-        <tr>
-            <td>Mata Kuliah</td>
-            <td><input type="text" name="matkul" value="<?php echo $d['matkul'] ?>"></td>
-        </tr>
-        <tr>
-            <td>Alamat</td>
-            <td>
-                <textarea name="alamat" cols="30" rows="5"><?php echo $d['alamat'] ?></textarea>
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <div style="display: flex; justify-content: space-between;">
-                <a href="tampil_dosen.php" type="button" class="btn btn-primary">KEMBALI</a>
-                <input class="btn btn-success" type="submit" value="SIMPAN">
-                </div>
-            </td>
-        </tr>
-        </table>
+        <div class="mb-3">
+            <label for="nim" class="form-label">NIDN</label>
+            <input type="hidden" class="form-control" name="id" value="<?php echo $d['id'] ?>">
+            <input type="text" class="form-control" name="nidn" value="<?php echo $d['nidn'] ?>">
+        </div>
+        <div class="mb-3">
+            <label for="nim" class="form-label">Nama</label>
+            <input type="text" class="form-control" name="nama" value="<?php echo $d['nama'] ?>">
+        </div>
+        <div class="mb-3">
+            <label for="nim" class="form-label">Mata Kuliah</label>
+            <input type="text" class="form-control" name="matkul" value="<?php echo $d['matkul'] ?>">
+        </div>
+        <div class="mb-3">
+            <label for="nim" class="form-label">Alamat</label>
+            <input type="text" class="form-control" name="alamat" value="<?php echo $d['alamat'] ?>">
+        </div>
+        <a href="tampil_mhs.php" type="submit" class="btn btn-primary">Kembali</a>
+        <input type="submit" class="btn btn-success" value="Simpan">
 <?php
 }
 ?>
