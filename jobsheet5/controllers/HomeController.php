1f9078de533d37c4221334db3282c43bb7780619
av <alifiaafifa14@gmail.com>
2023-12-05 09:30:07 +0700

    jobsheet 5

--- jobsheet5/controllers/HomeController.php
+++ jobsheet5/controllers/HomeController.php
@@ -6,7 +6,6 @@ class HomeController {
     public function mahasiswa() {
         header("location:http://localhost/jobsheet5/views/mahasiswa/index.php");
     }
-
     public function dosen() {
         header("location:http://localhost/jobsheet5/views/dosen/index.php");
     }
